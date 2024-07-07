14f6753a9a6ee187b8f3392538a150602010a3c1
Shubhashish Bhowmik <shuvobhowmik_cse11uits@yahoo.com>
2024-07-07 12:10:41 +0600

    Breeze and backend Layout Done

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,26 @@
 <?php
 
+use App\Http\Controllers\backend\DashboardController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('frontend.index');
+    return view('welcome');
 });
+
+/*Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard'); */
+
+Route::middleware(['auth', 'verified'])->group(function(){
+    Route::get('/dashboard', [DashboardController::class, 'Dashboard'])->name('dashboard');
+
+});
+
+/*Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});*/
+
+require __DIR__.'/auth.php';
